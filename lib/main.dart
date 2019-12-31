bbecd7fb982180f1f49a7042eebee60b13e02ce4
huangrui <richard34huang@hotmail.com>
2019-12-31 22:54:50 +0800

    完成一位整数加减法功能

--- lib/main.dart
+++ lib/main.dart
@@ -20,19 +20,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int n1 = 0;
+  int n2 = 0;
+  int operator = 0; // 0123,+-*/
+  int result = 0;
+  int expectResult = 0;
+
+  @override
+  void initState() {
+    _generateQuestion();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
-          color: Colors.grey,
           child: Column(
             children: <Widget>[
               _calculateView(),
-              Container(
-                height: 32,
-              ),
               _padView(),
             ],
           ),
@@ -42,38 +50,18 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget _calculateView() {
-    int n1;
-    int n2;
-    int result;
     String operatorStr;
-
-    bool oneMore = true;
-    while (oneMore) {
-      oneMore = false;
-
-      n1 = Random().nextInt(10);
-      n2 = Random().nextInt(10);
-      var operator = Random().nextInt(2);
-      result = 0;
-      if (operator == 0) {
-        result = n1 + n2;
-        operatorStr = '+';
-      } else if (operator == 1) {
-        if (n1 < n2) {
-          oneMore = true;
-          continue;
-        }
-        result = n1 - n2;
-        operatorStr = '-';
-      } else if (operator == 2) {
-        result = n1 * n2;
-        operatorStr = 'x';
-      } else {
-        result = n1 ~/ n2;
-        operatorStr = '/';
-      }
+    if (operator == 0) {
+      operatorStr = '+';
+    } else if (operator == 1) {
+      operatorStr = '-';
+    } else if (operator == 2) {
+      operatorStr = 'x';
+    } else {
+      operatorStr = '/';
     }
     return Container(
+      margin: EdgeInsets.all(32.0),
       child: Center(
         child: Text(
           '$n1 $operatorStr $n2 = $result',
@@ -117,9 +105,9 @@ class _MyAppState extends State<MyApp> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              _numPad(' '),
+              _numPad('Del'),
               _numPad('0'),
-              _numPad(' '),
+              _numPad('OK'),
             ],
           ),
         ],
@@ -128,18 +116,71 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget _numPad(String title) {
-    return Container(
-      color: Colors.white,
-      width: 64,
-      height: 64,
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(
-            fontSize: 32,
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          try {
+            int n = int.parse(title);
+            if (result <= 9) {
+              result = result * 10 + n;
+            }
+          } catch (e) {
+            if (title == 'OK') {
+              // 生成新的题目
+              if (result == expectResult) {
+                _generateQuestion();
+              }
+            } else if (title == 'Del') {
+              // 清空输入
+              result = 0;
+            }
+          }
+        });
+      },
+      padding: EdgeInsets.zero,
+      child: Container(
+        margin: EdgeInsets.all(1),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 1,
+          ),
+        ),
+        width: 128,
+        height: 128,
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 32,
+            ),
           ),
         ),
       ),
     );
+  }
+
+  _generateQuestion() {
+    n1 = 0;
+    n2 = 0;
+    operator = 0;
+    expectResult = 0;
+    result = 0;
+
+    while (true) {
+      operator = Random().nextInt(2);
+      n1 = Random().nextInt(10);
+      n2 = Random().nextInt(10);
+      if (operator == 0) {
+        expectResult = n1 + n2;
+      } else if (operator == 1) {
+        if (n1 < n2) {
+          continue;
+        } else {
+          expectResult = n1 - n2;
+        }
+      }
+      break;
+    }
   }
 }
